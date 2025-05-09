41d64216393c36dedc864d743088d75285e794a4
Seeth100 <isebas550@gmail.com>
2025-05-08 21:07:33 -0500

    4

--- funciones2.h
+++
@@ -1,9 +0,0 @@
-void copiarCadena(char destino[], char origen[]);
-int compararCadenas(char a[], char b[]);
-int buscarIndiceProducto(char nombre[]);
-void agregarProducto();
-void calcularTotal();
-void encontrarExtremos();
-void calcularPromedio();
-void buscarProducto();
-void mostrarMenu();
