d1ed8105219ac09654dd5cfc94cbc78b35ba68cc
Seeth100 <isebas550@gmail.com>
2025-05-08 20:52:39 -0500

    2

---
+++ Main1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "funciones1.h"
+
+int main() {
+    int opcion;
+
+    do {
+        mostrarMenu();
+        scanf("%d", &opcion);
+        fflush(stdin);
+
+        switch (opcion) {
+            case 1:
+                agregarProducto();
+                break;
+            case 2:
+                calcularTotal();
+                break;
+            case 3:
+                encontrarExtremos();
+                break;
+            case 4:
+                calcularPromedio();
+                break;
+            case 5:
+                buscarProducto();
+                break;
+            case 6:
+                printf("Saliendo del programa...\n");
+                break;
+            default:
+                printf("Opcion no valida, Intente de nuevo.\n");
+        }
+    } while (opcion != 6);
+
+    return 0;
+}
